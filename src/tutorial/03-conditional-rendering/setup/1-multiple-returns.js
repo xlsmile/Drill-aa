68f86f72c0dd4afd41a7dfcb7dfe149cd65280ba
Serge Seva <sergeseva@catalyst.net.nz>
2022-12-07 12:01:13 +1300

    Update the api fetch.

--- src/tutorial/03-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/03-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,37 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/vanpelt';
+
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState('default user');
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.status >= 200 && response.status <= 299) {
+          return response.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(response.statusText);
+        }
+      })
+      .then((user) => {
+        const { login } = user;
+        setUser(login);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>Error</h2>;
+  }
+  return <h2>{user}</h2>;
 };
 
 export default MultipleReturns;
