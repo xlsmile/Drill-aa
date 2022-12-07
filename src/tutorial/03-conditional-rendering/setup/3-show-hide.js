caa733b31dc0fafefbe1167faa20b183c3deebf3
Serge Seva <sergeseva@catalyst.net.nz>
2022-12-08 11:40:16 +1300

    Add show/hide drill.

--- src/tutorial/03-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/03-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,38 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <button className="btn" onClick={() => setShow(!show)}>
+        show/hide
+      </button>
+      {show && <Item />}
+    </>
+  );
+};
+
+const Item = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const checkWindowWidth = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', checkWindowWidth);
+    return () => {
+      window.removeEventListener('resize', checkWindowWidth);
+    };
+  }, []);
+
+  return (
+    <>
+      <h2>Window size:</h2>
+      <h3>{windowWidth} px</h3>
+    </>
+  );
 };
 
 export default ShowHide;
