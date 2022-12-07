633a0eb72d6cd89bad893800826184440c454c2e
Serge Seva <sergeseva@catalyst.net.nz>
2022-12-08 11:43:55 +1300

    Remove redundant code.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/03-conditional-rendering/setup/3-show-hide';
+import Setup from './tutorial/03-conditional-rendering/setup/2-short-circuit';
 function App() {
   return (
     <div className="container">
--- src/tutorial/03-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/03-conditional-rendering/setup/2-short-circuit.js
@@ -3,13 +3,12 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  const [text, setText] = useState('Toggle Error Display');
   const [isError, setIsError] = useState(false);
 
   return (
     <>
       <button className="btn" onClick={() => setIsError(!isError)}>
-        {text}
+        Toggle Error Display
       </button>
       {isError ? <h3>There is an error</h3> : <h3>There is no error</h3>}
     </>
