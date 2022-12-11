398ca6a05fd2aa0482db54aaf96c27edb023e11e
Serge Seva <sergeseva@catalyst.net.nz>
2022-12-12 11:51:44 +1300

    Complete Multiple inputs drill.

--- src/tutorial/04-forms/setup/2-multiple-inputs.js
+++ src/tutorial/04-forms/setup/2-multiple-inputs.js
@@ -1,41 +1,50 @@
 import React, { useState } from 'react';
 
 const ControlledInputs = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [person, setPerson] = useState({ name: '', age: '', email: '' });
   const [list, setList] = useState([]);
+
+  const handleInputChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setPerson({ ...person, [name]: value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (name && email) {
-      const newitem = { id: new Date().getTime().toString(), name, email };
-      setList((list) => {
-        return [...list, newitem];
-      });
-      setName('');
-      setEmail('');
+    if (person.name && person.age && person.email) {
+      const newPerson = { ...person, id: new Date().getTime().toString() };
+      setList([...list, newPerson]);
+      setPerson({ name: '', age: '', email: '' });
     }
   };
 
   return (
     <article>
       <h1>React Multiple Inputs</h1>
-      <form className="form" onSubmit={handleSubmit}>
+      <form className="form">
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+          <input type="text" id="name" name="name" value={person.name} onChange={handleInputChange} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="age">Age</label>
+          <input type="number" id="age" name="age" value={person.age} onChange={handleInputChange} />
         </div>
         <div className="form-control">
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="text" id="email" name="email" value={person.email} onChange={handleInputChange} />
         </div>
-        <button>Submit</button>
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
       </form>
       {list.map((listitem) => {
-        const { id, name, email } = listitem;
+        const { id, name, email, age } = listitem;
         return (
           <div className="item" key={id}>
-            <h4>{name}</h4>
+            <h4>Name: {name}</h4>
+            <h4>Age: {age}</h4>
             <a href={`mailto:${email}`}>{email}</a>
           </div>
         );
