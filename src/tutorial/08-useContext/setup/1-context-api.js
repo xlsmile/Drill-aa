f4396c9742d47ead35f715cad7cb7df545c68d8b
Serge Seva <sergeseva@catalyst.net.nz>
2022-12-20 10:31:30 +1300

    Add Context.

--- src/tutorial/08-useContext/setup/1-context-api.js
+++ src/tutorial/08-useContext/setup/1-context-api.js
@@ -1,35 +1,40 @@
 import React, { useState, useContext } from 'react';
 import { data } from '../../../data';
 
-// more components
-// fix - context api, redux (for more complex cases)
+const TestContext = React.createContext();
+
+// TestContext gives us access to 2 components: Provider and Consumer
 
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
+
   const removePerson = (id) => {
     setPeople((people) => {
       return people.filter((person) => person.id !== id);
     });
   };
+
   return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+    <TestContext.Provider value={{ removePerson }}>
+      <h3>Context API</h3>
+      <List people={people} />
+    </TestContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = ({ people }) => {
   return (
     <>
       {people.map((person) => {
-        return <SinglePerson key={person.id} {...person} removePerson={removePerson} />;
+        return <SinglePerson key={person.id} {...person} />;
       })}
     </>
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  const { removePerson } = useContext(TestContext);
+
   return (
     <div className="item">
       <h4>{name}</h4>
